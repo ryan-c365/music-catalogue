4b248ea0c140a5af85b7ecf2fc3ebd0aa62140eb
Ryan <ryan.ching@compliance365.com>
2026-06-12 11:20:06 -0400

    Added DTO, data annotation, validation

---
+++ music-catalogue-app/music-catalogue-app/Models/SongDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace music_catalogue_app.Models {
+    public class SongDTO {
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100)]
+        public string title { get; set; } = "";
+
+        [Required(ErrorMessage = "Artist is required")]
+        [StringLength(100)]
+        public string artist { get; set; } = "";
+
+    }
+
+}
